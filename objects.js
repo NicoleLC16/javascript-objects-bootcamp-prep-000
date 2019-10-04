65e0626d73c76bb611203fd1493356937e816143
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-04 20:29:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,6 +9,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist (playlist, artistName, songTitle) {
-  delete playlist.
+  delete playlist.artistName;
+  return playlist;
   
 }
