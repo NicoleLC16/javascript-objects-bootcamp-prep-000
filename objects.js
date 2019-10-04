bca6cbfb4dbdc2313666064a81e9d9c6978b594a
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-04 20:19:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = { Chainsmokers: 'Call You Mine' };
 
 function updatePlaylist (playlist, artistName, songTitle) {
   Object.assign
-  return Object.assign({}, playlist, {Alison: 'Slowdive'});
+  return Object.assign({}, playlist, {artistName: songTitle});
 }
 
