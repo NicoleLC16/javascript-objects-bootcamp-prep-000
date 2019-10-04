25f12f751a66ef92af484e3f2b90ec3ad02831b3
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-04 20:26:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,5 +3,7 @@ var playlist = { artistName: 'SongTitle' };
 function updatePlaylist (playlist, artistName, songTitle) {
   
   Object.assign ({}, playlist, {artistName: songTitle});
+  
+  return playlist;
 }
 
